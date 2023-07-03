5eb937393f8fc36570437a358ecda2c5e7261c80
arjun-rishi-2004 <arjunrishi478@gmail.com>
2023-07-03 19:26:05 +0530

    map,uuid done

--- src/App.js
+++ src/App.js
@@ -1,13 +1,38 @@
 import './index.css';
 import Employee from './components/Employee';
 import { useState } from 'react';
-
+import { v4 as uuidv4 } from 'uuid';
 function App() {
   const [role,setrole] = useState('dev')
   // console.log("Lets show the employee")
   let showEmp=true;
   // let role="dev"
-  con
+  const [employee,setEmployee]=useState(
+    [
+      {
+        name:"Arjun",
+        role:"Developer",
+        img:"https://akns-images.eonline.com/eol_images/Entire_Site/2019414/rs_600x600-190514063109-600-George-Clooney-Batman-LT-051419-GettyImages-607408286.jpg?fit=around%7C600:600&output-quality=90&crop=600:600;center,top"
+      },
+      {
+        name:"Rishi",
+        role:"Manager",
+        img:"https://images.pexels.com/photos/4061662/pexels-photo-4061662.jpeg?auto=compress&cs=tinysrgb&w=400"
+      }, {
+        name:"Bruce",
+        role:"Product Manager",
+        img:"https://akns-images.eonline.com/eol_images/Entire_Site/2019414/rs_600x600-190514063109-600-George-Clooney-Batman-LT-051419-GettyImages-607408286.jpg?fit=around%7C600:600&output-quality=90&crop=600:600;center,top"
+      }, {
+        name:"Stark",
+        role:"Technician",
+        img:"https://images.pexels.com/photos/11876188/pexels-photo-11876188.jpeg?auto=compress&cs=tinysrgb&w=400"
+      }, {
+        name:"Arjun",
+        role:"Network Engineer",
+        img:"https://akns-images.eonline.com/eol_images/Entire_Site/2019414/rs_600x600-190514063109-600-George-Clooney-Batman-LT-051419-GettyImages-607408286.jpg?fit=around%7C600:600&output-quality=90&crop=600:600;center,top"
+      }
+    ]
+  )
   return (
     <div className="App">
        
@@ -20,15 +45,19 @@ function App() {
         setrole(e.target.value)
       }}/>
      <div className='flex flex-wrap'>
-      <Employee name="Arjun" role="Developer" img="https://akns-images.eonline.com/eol_images/Entire_Site/2019414/rs_600x600-190514063109-600-George-Clooney-Batman-LT-051419-GettyImages-607408286.jpg?fit=around%7C600:600&output-quality=90&crop=600:600;center,top"/>
-      <Employee name="Rishi" role={role} img="https://akns-images.eonline.com/eol_images/Entire_Site/2019414/rs_600x600-190514063109-600-George-Clooney-Batman-LT-051419-GettyImages-607408286.jpg?fit=around%7C600:600&output-quality=90&crop=600:600;center,top" />
-      <Employee/>
-      <Employee name="Arjun" role="Developer" img="https://akns-images.eonline.com/eol_images/Entire_Site/2019414/rs_600x600-190514063109-600-George-Clooney-Batman-LT-051419-GettyImages-607408286.jpg?fit=around%7C600:600&output-quality=90&crop=600:600;center,top"/>
-      <Employee name="Rishi" role={role} img="https://akns-images.eonline.com/eol_images/Entire_Site/2019414/rs_600x600-190514063109-600-George-Clooney-Batman-LT-051419-GettyImages-607408286.jpg?fit=around%7C600:600&output-quality=90&crop=600:600;center,top" />
-      <Employee/>
-      <Employee name="Arjun" role="Developer" img="https://akns-images.eonline.com/eol_images/Entire_Site/2019414/rs_600x600-190514063109-600-George-Clooney-Batman-LT-051419-GettyImages-607408286.jpg?fit=around%7C600:600&output-quality=90&crop=600:600;center,top"/>
-      <Employee name="Rishi" role={role} img="https://akns-images.eonline.com/eol_images/Entire_Site/2019414/rs_600x600-190514063109-600-George-Clooney-Batman-LT-051419-GettyImages-607408286.jpg?fit=around%7C600:600&output-quality=90&crop=600:600;center,top" />
-      <Employee/>
+            {employee.map((emp)=>{
+            // console.log(uuidv4())
+               
+              return (
+
+              <Employee 
+               key={uuidv4()}
+              name={emp.name} 
+               role={emp.role}
+               img={emp.img} />
+              )
+            })
+            }
       </div>
 </>
       ):(
--- src/components/Employee.js
+++ src/components/Employee.js
@@ -1,5 +1,5 @@
 function Employee(props){
-    
+
 return(
     
     <div class="m-2 py-8 px-8 max-w-sm  bg-white rounded-xl shadow-lg space-y-2 sm:py-4 sm:flex sm:items-center sm:space-y-0 sm:space-x-6">
@@ -11,7 +11,7 @@ return(
       <p class="text-slate-500 font-medium">
 {props.role}      </p>
     </div>
-    <button class="px-4 py-1 text-sm text-purple-600 font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2">Message</button>
+    <button class="px-4 py-1 text-sm text-purple-600 font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2">Update</button>
   </div>
 </div>
 
